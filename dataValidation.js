68d86d2b850d096bc5219c9566ad71edb1587a7d
Rafal <rav.waszak@gmail.com>
2021-12-07 13:52:48 +0100

    tymczasowa walidacja

---
+++ dataValidation.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('form');
+const firstName = document.getElementById('firstName');
+
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const firstNameValue = firstName.value.trim();
+
+	
+	if(firstNameValue === '') {
+		setErrorFor(firstName, 'Username cannot be blank');
+	} else {
+		setSuccessFor(firstName);
+	}
+
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
